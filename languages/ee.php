ba359902beb5e8c04f7d38e27ef1e87d70659f9a
tangledhelix <devnull@localhost>
2002-06-02 01:24:29 +0000

    adding new file

---
+++ languages/ee.php
@@ -0,0 +1,27 @@
+
+// Estonian - courtesy of Jan Jasinski <jan@sps.ee>
+$mig_config['lang_lib']['ee'] = array (
+    'am'            => 'AM',
+    'pm'            => 'PM',
+    'backhome'     => 'tagasi',
+    'bytes'        => '&nbsp;baiti',
+    'flash_used'   => 'kasutatud&nbsp;v&auml;lku',
+    'main'         => 'Avaleht',
+    'must_auth'    => 'Vaatamiseks&nbsp;tuleb&nbsp;sisestada&nbsp;'
+                    . 'kasutajatunnus&nbsp;ja&nbsparool',
+    'nextimage'    => 'j&auml;rgmine&nbspilt',
+    'no_contents'  => 'Kataloog&nbsp;on&nbsp;t&uuml;hi',
+    'previmage'    => 'eelmine&nbspilt',
+    'thumbview'    => 'tagasi&nbsp;ikoonide&nbsp;juurde',
+    // total_images is special.  It has three elements you can use:
+    //     %t :    Total images in folder
+    //     %s :    First image shown this page
+    //     %e :    Last image shown this page
+    'total_images' => 'N&auml;itan&nbspilte&nbsp;%s-%e&nbsp;(%t)&nbsp;',
+    'up_one'       => 'tase&nbsp;kõrgemale',
+    'month'        => array ( '01' => 'Jan', '02' => 'Feb', '03' => 'Mar',
+                              '04' => 'Apr', '05' => 'May', '06' => 'Jun',
+                              '07' => 'Jul', '08' => 'Aug', '09' => 'Sep',
+                              '10' => 'Oct', '11' => 'Nov', '12' => 'Dec')
+);
+
