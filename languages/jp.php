1d10b2f9d396b320863dc4a72bdd0d979c635f09
tangledhelix <devnull@localhost>
2002-06-06 06:20:17 +0000

    adding new file

---
+++ languages/jp.php
@@ -0,0 +1,29 @@
+
+$mig_config['lang_lib']['jp'] = array (
+    'am'            => '午前',
+    'pm'            => '午後',
+    'backhome'      => 'ホームに戻る -',
+    'bytes'         => '&nbsp;バイト',
+    'flash_used'    => 'Flush Used',
+    'main'          => 'メインページ',
+    'must_auth'     => 'ユーザ名とパスワードを入力してください'
+                     . ' Enter を押してください',
+    'nextimage'     => '次の画像',
+    'no_contents'   => '画像がありません',
+    'previmage'     => '前の画像',
+    'thumbview'     => '一覧に戻る',
+    // total_images is special.  It has three elements you can use:
+    //     %t :    Total images in folder
+    //     %s :    First image shown this page
+    //     %e :    Last image shown this page
+    'total_images'  => '表示中 %s 枚目 - %e 枚目 /'
+                     . '全画像数 %t 枚<br>',
+    'up_one'        => '一つ上に行く',
+    'month'      => array ( '01' => '1 月', '02' => '2 月',
+                            '03' => '3 月', '04' => '4 月',
+                            '05' => '5 月', '06' => '6 月',
+                            '07' => '7 月', '08' => '8 月',
+                            '09' => '9 月', '10' => '10 月',
+                            '11' => '11 月', '12' => '12 月' )
+);
+
