7f20f62d518308c0d21361fba309c9a9a053522e
tangledhelix <devnull@localhost>
2002-06-07 21:40:25 +0000

    adding new file

---
+++ functions/convertIncludePath.php
@@ -0,0 +1,15 @@
+
+// convertIncludePath() - Converts the path used by include() if needed.
+//                        (Not normally needed, but some installs of PHP
+//                        demand this).
+
+function convertIncludePath ( $flag, $path='', $regex, $new )
+{
+    if ($flag) {
+        $path = ereg_replace($regex, $new, $path);
+    }
+
+    return($path);
+
+}   // -- End of convertIncludePath()
+
