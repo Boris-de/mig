ddd7b2d4389f21cb39c23bb01b044d3d094493c2
tangledhelix <devnull@localhost>
2002-07-06 22:51:06 +0000

    adding new function

---
+++ functions/buildTable.php
@@ -0,0 +1,15 @@
+
+// buildTable() - frames stuff in HTML table code so it's ready to use
+//                as a stand-alone element in a template.
+
+function buildTable ( $input, $tableclass, $tablesummary )
+{
+    $retval = '<table summary="' . $tablesummary
+            . '" border="0" cellpadding="0" cellspacing="0"><tbody>'
+            . "\n " . '<tr><td class="' . $tableclass . '">'
+            . $input . "\n " . '</td></tr>' . "\n" . '</table>';
+
+    return $retval;
+
+}   // -- End of buildTable()
+
