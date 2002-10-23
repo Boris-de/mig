9c546d870f809f597f2f9f8988b7c9e49851fc86
tangledhelix <devnull@localhost>
2002-10-23 01:57:48 +0000

    *** empty log message ***

---
+++ xoops/language/english/modinfo.php
@@ -0,0 +1,9 @@
+<?php
+// Module Info
+
+// The name of this module
+define("_MI_MIG_NAME","Photo Album");
+// A brief description of this module
+define("_MI_MIG_DESC","My Image Gallery");
+
+?>
---
+++ xoops/xoops_version.php
@@ -0,0 +1,21 @@
+<?php
+// $Id$
+
+$modversion['name'] = _MI_MIG_NAME;
+$modversion['version'] = 1.00;
+$modversion['description'] = _MI_MIG_DESC;
+$modversion['credits'] = "Wrapping Mig Copyright 2000-2002 Daniel M. Lowe <dan@tangledhelix.com> ";
+$modversion['author'] = "Marius Scurtescu";
+$modversion['help'] = "";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "mig_logo.jpg";
+$modversion['dirname'] = "mig";
+
+//Admin things
+$modversion['hasAdmin'] = 0;
+$modversion['adminmenu'] = "";
+
+// Menu
+$modversion['hasMain'] = 1;
+?>
