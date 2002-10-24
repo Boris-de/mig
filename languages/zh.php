dcfea86255846db42b47240bad02ed237a441d9e
tangledhelix <devnull@localhost>
2002-10-24 02:23:14 +0000

    new lang

---
+++ languages/zh.php
@@ -0,0 +1,29 @@
+
+// Traditional Chinese (big5)
+// Courtesy of Belle Liu <belle@belleliu.com>
+$mig_config['lang_lib']['zh'] = array (
+     'am'            => '上午',
+     'pm'            => '下午',
+     'backhome'      => '回到',
+     'bytes'         => '&nbsp;位元',
+     'flash_used'    => '用閃光燈',
+     'main'          => '主頁',
+     'must_auth'     => '你必須輸入有效的用戶名稱及密碼才可進入',
+     'nextimage'     => '下一張圖片',
+     'no_contents'   => '沒有內容',
+     'previmage'     => '上一張圖片',
+     'thumbview'     => '回到縮圖檢視',
+     // total_images is special.  It has three elements you can use:
+     //     %t :    Total images in folder
+     //     %s :    First image shown this page
+     //     %e :    Last image shown this page
+     'total_images'  => '顯示%t張圖片中的第%s-%e張',
+     'up_one'        => '回上一層',
+     'month'         => array ( '01' => '一月', '02' => '二月',
+                                '03' => '三月', '04' => '四月',
+                                '05' => '五月', '06' => '六月',
+                                '07' => '七月', '08' => '八月',
+                                '09' => '九月', '10' => '十月',
+                                '11' => '十一月', '12' => '十二月' )
+);
+
