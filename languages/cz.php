439bb37890aa72fe5d6d5a4f847a60290bdc2712
tangledhelix <devnull@localhost>
2002-12-07 01:29:18 +0000

    Adding cz language

---
+++ languages/cz.php
@@ -0,0 +1,33 @@
+
+
+// Czech - encoding iso8859-2
+// courtesy of Pavel XPJ Jisl <pavel@cetoraz.info>
+
+$mig_config["lang_lib"]["cz"] = array (
+       "am"            => "AM",
+       "pm"            => "PM",
+       "backhome"      => "zpět&nbsp;domů",
+       "bytes"         => "&nbsp;bytů",
+       "flash_used"    => "flash&nbsp;použit",
+       "largelink"     => "zobrazit&nbsp;velký&nbsp;obrázek",
+       "largeview"     => "zpět&nbsp;na&nbsp;zobrazení&nbsp;malých&nbsp;obrázků",
+       "main"          => "Hlavní",
+       "must_auth"     => "Musíte&nbsp;vložit&nbsp;správné&nbsp;jméno&nbsp;a"
+                        . "&nbsp;heslo&nbsp;pro&nbsp;vstup",
+       "nextimage"     => "následující&nbsp;obrázek",
+       "no_contents"   => "Žádné&nbsp;obrázky.",
+       "previmage"     => "předchozí&nbsp;obrázek",
+       "thumbview"     => "zpět&nbsp;na&nbsp;zobrazení&nbsp;náhledů",
+       // total_images is special.  It has three elements you can use:
+       //     %t :    Total images in folder
+       //     %s :    First image shown this page
+       //     %e :    Last image shown this page
+       "total_images"  => "Zobrazuji&nbsp;obrázky&nbsp;%s-%e&nbsp;z&nbsp;%t"
+                        . "&nbsp;celkem<br />",
+       "up_one"        => "o&nbsp;úroveň&nbsp;výše",
+       "month"         => array ( "01" => "Led", "02" => "Úno", "03" => "Bře",
+                                  "04" => "Dub", "05" => "Kvě", "06" => "Čen",
+                                  "07" => "Čec", "08" => "Srp", "09" => "Zář",
+                                  "10" => "Říj", "11" => "Lis", "12" => "Pro" )
+);
+
