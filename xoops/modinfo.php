10e782f8175ac1e8ca190306ccfcc21a0e9c0bcb
tangledhelix <devnull@localhost>
2003-02-01 20:46:01 +0000

    No log message.

--- xoops/modinfo.php
+++ xoops/modinfo.php
@@ -2,8 +2,8 @@
 // Module Info
 
 // The name of this module
-define("_MI_MIG_NAME","Photo Album");
+define('_MI_MIG_NAME','Photo Album');
 // A brief description of this module
-define("_MI_MIG_DESC","My Image Gallery");
+define('_MI_MIG_DESC','My Image Gallery');
 
 ?>
--- xoops/xoops_version.php
+++ xoops/xoops_version.php
@@ -1,21 +1,21 @@
 <?php
 // $Id$
 
-$modversion["name"] = _MI_MIG_NAME;
-$modversion["version"] = 1.00;
-$modversion["description"] = _MI_MIG_DESC;
-$modversion["credits"] = "Wrapping Mig Copyright 2000-2002 Daniel M. Lowe <dan@tangledhelix.com> ";
-$modversion["author"] = "Marius Scurtescu";
-$modversion["help"] = "";
-$modversion["license"] = "GPL see LICENSE";
-$modversion["official"] = 0;
-$modversion["image"] = "mig_logo.jpg";
-$modversion["dirname"] = "mig";
+$modversion['name'] = _MI_MIG_NAME;
+$modversion['version'] = 1.00;
+$modversion['description'] = _MI_MIG_DESC;
+$modversion['credits'] = 'Wrapping Mig Copyright 2000-2002 Daniel M. Lowe <dan@tangledhelix.com> ';
+$modversion['author'] = 'Marius Scurtescu';
+$modversion['help'] = '';
+$modversion['license'] = 'GPL see LICENSE';
+$modversion['official'] = 0;
+$modversion['image'] = 'mig_logo.jpg';
+$modversion['dirname'] = 'mig';
 
 //Admin things
-$modversion["hasAdmin"] = 0;
-$modversion["adminmenu"] = "";
+$modversion['hasAdmin'] = 0;
+$modversion['adminmenu'] = '';
 
 // Menu
-$modversion["hasMain"] = 1;
+$modversion['hasMain'] = 1;
 ?>
