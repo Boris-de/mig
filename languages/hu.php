69cde5cdbe970df3cdb77ee92752b16bd8a3a852
tangledhelix <devnull@localhost>
2004-06-23 14:34:48 +0000

    Adding a new file

---
+++ languages/hu.php
@@ -0,0 +1,44 @@
+<?
+
+// Courtesy of Tamas Buti <buty@axelero.hu>
+$mig_config['lang_lib']['hu'] = array (
+    'am'            => 'AM',
+    'pm'            => 'PM',
+    'backhome'      => 'Vissza:',
+    'bytes'         => '&nbsp;byte',
+    'flash_used'    => 'Vaku&nbsp;bekapcsolva',
+    'largelink'     => 'Teljes&nbsp;m&eacute;ret&ucirc;&nbsp;k&eacute;p',
+    'largeview'     => 'Vissza&nbsp;a&nbsp;norm&aacute;l&nbsp;m'
+                     . '&eacute;rethez',
+    'main'          => 'F&otilde;mappa',
+    'must_auth'     => '&Eacute;rv&eacute;nyes&nbsp;jelsz&oacute;t&nbsp;'
+                     . '&eacute;s&nbsp;felhaszn&aacute;l&oacute;nevet&nbsp;'
+                     . 'kell&nbsp;megadni&nbsp;a&nbsp;bel&eacute;p&eacute;'
+                     . 'shez',
+    'nextimage'     => 'K&ouml;vetkez&otilde;',
+    'no_contents'   => 'Nincs&nbsp;k&eacute;p.',
+    'previmage'     => 'El&otilde;z&otilde;',
+    'thumbview'     => 'Thumbnail&nbsp;k&eacute;pek',
+    // total_images is special.  It has three elements you can use:
+    //     %t :    Total images in folder
+    //     %s :    First image shown this page
+    //     %e :    Last image shown this page
+    'total_images'  => '%s-%e&nbsp;k&eacute;pek&nbsp;%t'
+                     . '&nbsp;az&nbsp;&ouml;sszesb&otilde;l<br />',
+    'up_one'        => 'Egy&nbsp;mapp&aacute;t&nbsp;fel',
+    'month'         => array ( '01' => 'Jan',
+                               '02' => 'Feb',
+                               '03' => 'M&aacute;rc',
+                               '04' => '&Aacute;pr',
+                               '05' => 'M&aacute;j',
+                               '06' => 'J&uacute;n',
+                               '07' => 'J&uacute;l',
+                               '08' => 'Aug',
+                               '09' => 'Szept',
+                               '10' => 'Okt',
+                               '11' => 'Nov',
+                               '12' => 'Dec'
+                             )
+);
+
+?>
