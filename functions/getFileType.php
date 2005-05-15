23be18740968a545b86d572c5da3ee6e9b25c1ff
Boris Wachtmeister <boris_w@users.sf.net>
2005-05-15 12:59:53 +0000

    added some

--- functions/getFileType.php
+++ functions/getFileType.php
@@ -15,26 +15,33 @@ function getFileType ( $filename )
         case 'jpg':
         case 'gif':
         case 'png':
-        case 'jpeg':            // Alternate JPEG
-        case 'jpe':             // Alternate JPEG
+        case 'jpeg': case 'jpe':// Alternate JPEG
 
             return 'image';
             break;
 
         case 'mov':             // Apple Quicktime
-        case 'avi':             // Microsoft AVI
-        case 'mpg':             // MPEG video
-        case 'mpeg':            // Alternate MPEG video
+        case 'avi':             // AVI-container
+        case 'mpg': case 'mpeg':// MPEG video
         case 'wmv':             // Windows Media video
         case 'mp4':             // MPEG-4 video
+        case 'swf':             // Shockwave Flash
+        case 'flv':             // Flash Video
+        case 'rm':              // Realvideo
+        case 'divx':            // DivX
 
             return 'video';
             break;
 
         case 'mp3':             // MPEG-3 audio
-        case 'wav':             // Microsoft WAV audio
+        case 'wav':             // WAV audio
         case 'ra':              // Realaudio
         case 'ram':             // Realaudio
+        case 'wma':             // Microsoft Media Audio
+        case 'ogg':             // ogg-container (flac, vorbis)
+        case 'flac':            // Free Lossless Audio Codec
+        case 'aac':             // Advanced Audio Coding
+        case 'mpc': case 'mp+': // Musepack
 
             return 'audio';
             break;
