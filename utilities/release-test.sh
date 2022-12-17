d7197a82d6065f685659ab4fc06b28e2f3983a4b
Boris Wachtmeister <boris-code@gmx.com>
2022-12-17 11:07:55 +0100

    update release test script

--- utilities/release-test.sh
+++ utilities/release-test.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-set -e
+set -e -u
 
 die() {
   echo "${@}" 1>&2
   exit 1
+}
+
+cleanup() {
+  echo "Cleaning up"
+  rm -rf "${TEMP_DIR}"
 }
 
 test -z "${1}" && die "usage: ${0} archive"
@@ -12,15 +17,14 @@ ARCHIVE=${1}
 PORT=${2-"8000"}
 
 TEMP_DIR=$(mktemp -d)
+trap cleanup EXIT
 
 tar xf "${ARCHIVE}" -C "${TEMP_DIR}"
 mv "${TEMP_DIR}"/*/* "${TEMP_DIR}"
+cp "${TEMP_DIR}/config.php.default" "${TEMP_DIR}/config.php"
 
 "$(dirname "${0}")/create-random-album.sh" "${TEMP_DIR}/albums/"
 
 echo "Running server on localhost:${PORT}, use ctrl-c to abort testing"
 
-php --server "localhost:${PORT}" --docroot "${TEMP_DIR}" || true
-
-echo "Cleaning up"
-rm -r "${TEMP_DIR}"
+php --server "localhost:${PORT}" --docroot "${TEMP_DIR}"
